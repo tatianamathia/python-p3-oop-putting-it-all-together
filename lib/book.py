29c417ddcf35881c73d30128cc9c046552c2f8f7
Tatiana Mathia <mathiatatiana@gmail.com>
2024-05-24 11:30:57 +0300

    putting it all together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title="", page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+class TestBook:
+    def test_can_turn_page(self):
+        '''outputs "Flipping the page...wow, you read fast!" when method turn_page() is called'''
+        book = Book("The World According to Garp", 69)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        book.turn_page()
+        assert captured_out.getvalue() == "Flipping the page...wow, you read fast!\n"
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand="", size=0):
+        self.brand = brand
+        self.size = size
+        self.condition = "New" 
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New" 
+
+class TestShoe:
+    def test_can_cobble(self):
+        '''says that the shoe has been repaired.'''
+        stan_smith = Shoe("Adidas", 9)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        stan_smith.cobble()
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue().strip() == "The shoe has been repaired."
